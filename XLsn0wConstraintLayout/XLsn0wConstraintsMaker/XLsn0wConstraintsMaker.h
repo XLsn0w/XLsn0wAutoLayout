8a918141035af470adbf4c9d2518ece223b691ec
xlsn0w <xlsn0wios@gmail.com>
2018-05-30 16:03:49 +0800

    update

--- XLsn0wConstraintLayout/XLsn0wConstraintsMaker/XLsn0wConstraintsMaker.h
+++ XLsn0wConstraintLayout/XLsn0wConstraintsMaker/XLsn0wConstraintsMaker.h
@@ -33,76 +33,33 @@ typedef void (^SpaceToSuperView)(UIEdgeInsets insets);
 @property (nonatomic, copy, readonly) WidthHeight widthValue;
 @property (nonatomic, copy, readonly) WidthHeight heightValue;
 
-
-/* 设置x、y、width、height、centerX、centerY 值 */
-
-/** x值，参数为“(CGFloat)”  */
 @property (nonatomic, copy, readonly) Margin xIs;
-/** y值，参数为“(CGFloat)”  */
 @property (nonatomic, copy, readonly) Margin yIs;
-/** centerX值，参数为“(CGFloat)”  */
 @property (nonatomic, copy, readonly) Margin centerXIs;
-/** centerY值，参数为“(CGFloat)”  */
 @property (nonatomic, copy, readonly) Margin centerYIs;
 
-
-
-
-/* 设置最大宽度和高度、最小宽度和高度 */
-
-/** 最大宽度值，参数为“(CGFloat)”  */
 @property (nonatomic, copy, readonly) WidthHeight maxWidthIs;
-/** 最大高度值，参数为“(CGFloat)”  */
 @property (nonatomic, copy, readonly) WidthHeight maxHeightIs;
-/** 最小宽度值，参数为“(CGFloat)”  */
 @property (nonatomic, copy, readonly) WidthHeight minWidthIs;
-/** 最小高度值，参数为“(CGFloat)”  */
 @property (nonatomic, copy, readonly) WidthHeight minHeightIs;
 
-
-
-/* 设置和某个参照view的边距相同 */
-
-/** 左间距与参照view相同，参数为“(View)”  */
 @property (nonatomic, copy, readonly) MarginEqualToView left_equalTo;
-/** 右间距与参照view相同，参数为“(View)”  */
 @property (nonatomic, copy, readonly) MarginEqualToView right_equalTo;
-/** 顶部间距与参照view相同，参数为“(View)”  */
 @property (nonatomic, copy, readonly) MarginEqualToView top_equalTo;
-/** 底部间距与参照view相同，参数为“(View)”  */
 @property (nonatomic, copy, readonly) MarginEqualToView bottom_equalTo;
-/** centerX与参照view相同，参数为“(View)”  */
 @property (nonatomic, copy, readonly) MarginEqualToView centerX_equalTo;
-/** centerY与参照view相同，参数为“(View)”  */
 @property (nonatomic, copy, readonly) MarginEqualToView centerY_equalTo;
+@property (nonatomic, copy, readonly) Offset offset;/// 偏移量
 
-
-
-/*  设置宽度或者高度等于参照view的多少倍 */
-
-/** 宽度是参照view宽度的多少倍，参数为“(View, CGFloat)” */
 @property (nonatomic, copy, readonly) WidthHeightEqualToView widthRatioToView;
-/** 高度是参照view高度的多少倍，参数为“(View, CGFloat)” */
 @property (nonatomic, copy, readonly) WidthHeightEqualToView heightRatioToView;
-/** 设置一个view的宽度和它的高度相同，参数为空“()” */
 @property (nonatomic, copy, readonly) SameWidthHeight widthEqualToHeight;
-/** 设置一个view的高度和它的宽度相同，参数为空“()” */
 @property (nonatomic, copy, readonly) SameWidthHeight heightEqualToWidth;
-/** 自适应高度，传入高宽比值，label可以传0实现文字高度自适应 */
 @property (nonatomic, copy, readonly) AutoHeightWidth autoHeightRatio;
 
-/** 自适应宽度，参数为宽高比值 */
 @property (nonatomic, copy, readonly) AutoHeightWidth autoWidthRatio;
 
-
-
-/* 填充父view(快捷方法) */
-
-/** 传入UIEdgeInsetsMake(top, left, bottom, right)，可以快捷设置view到其父view上左下右的间距  */
 @property (nonatomic, copy, readonly) SpaceToSuperView spaceToSuperView;
-
-/** 设置偏移量，参数为“(CGFloat value)，目前只有带有equalToView的方法可以设置offset” */
-@property (nonatomic, copy, readonly) Offset offset;
 
 @property (nonatomic, weak) UIView *needsAutoResizeView;
 
